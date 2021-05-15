a1f4544f1e59f6725748dd12ba06d5a10b70abfc
Gurveen <gurveens11@gmail.com>
2021-05-15 15:27:18 +0530

    Add show password icon

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Input = ({
 	id,
@@ -10,20 +10,54 @@ const Input = ({
 	type,
 	value,
 }) => {
-	return (
-		<div className="input-item">
-			<label htmlFor={id}>{label}</label>
-			<input
-				type={type}
-				id={id}
-				name={name}
-				placeholder={placeholder}
-				required={required}
-				value={value}
-				onChange={onChange}
-			/>
-		</div>
-	)
+	const passwordRef = useRef()
+
+	//Handle show password
+	const mouseHandler = (e) => {
+		if (e.type === 'mousedown') {
+			passwordRef.current.type = 'text'
+		} else {
+			passwordRef.current.type = 'password'
+		}
+	}
+
+	if (id === 'password') {
+		return (
+			<div className="input-item">
+				<i
+					className="fas fa-eye"
+					onMouseDown={mouseHandler}
+					onMouseUp={mouseHandler}
+				></i>
+				<label htmlFor={id}>{label}</label>
+				<input
+					ref={passwordRef}
+					type={type}
+					id={id}
+					name={name}
+					placeholder={placeholder}
+					required={required}
+					value={value}
+					onChange={onChange}
+				/>
+			</div>
+		)
+	} else {
+		return (
+			<div className="input-item">
+				<label htmlFor={id}>{label}</label>
+				<input
+					type={type}
+					id={id}
+					name={name}
+					placeholder={placeholder}
+					required={required}
+					value={value}
+					onChange={onChange}
+				/>
+			</div>
+		)
+	}
 }
 
 export default Input
